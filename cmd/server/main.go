0025df8221b988ba80a882a23ac968c281c4b683
Michael <michael.icwang@gmail.com>
2021-07-04 23:55:23 +0800

    Improve idempotency API
    - Per API cache
    - Remove race condition

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -20,8 +20,8 @@ var (
 )
 
 type application struct {
-	db      *sql.DB
-	ikCache *cache.Cache
+	db *sql.DB
+	//ikCache *cache.Cache
 }
 
 func main() {
@@ -41,8 +41,8 @@ func main() {
 	defer db.Close()
 
 	app := &application{
-		db:      db,
-		ikCache: cache.New(10*time.Second, time.Minute),
+		db: db,
+		//ikCache: cache.New(10*time.Second, time.Minute),
 	}
 
 	mux := http.NewServeMux()
@@ -148,18 +148,6 @@ func (app *application) writeJson(w http.ResponseWriter, r *http.Request, status
 	return nil
 }
 
-type Response struct {
-	Ready  bool // if response ready to be used
-	Header http.Header
-	Status int
-	Body   []byte
-}
-
-func dump(c *cache.Cache) {
-	bb, _ := json.MarshalIndent(c.Items(), "", "  ")
-	log.Printf("ikCache: %s\n", bb)
-}
-
 func logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s\n", r.Method, r.URL.Path)
@@ -174,65 +162,110 @@ func (app *application) responseError(w http.ResponseWriter, r *http.Request, ms
 	http.Error(w, msg, status)
 }
 
-type idempotencyWriter struct {
+type respCatcher struct {
 	http.ResponseWriter
 	body       *bytes.Buffer
 	statusCode int
 }
 
-func (w *idempotencyWriter) WriteHeader(code int) {
-	fmt.Printf("WriteHeader code: %d\n", code)
+func (w *respCatcher) WriteHeader(code int) {
 	w.statusCode = code
 	w.ResponseWriter.WriteHeader(code)
 }
 
-func (w *idempotencyWriter) Write(p []byte) (n int, err error) {
-	fmt.Printf("Write p: %s\n", string(p))
+func (w *respCatcher) Write(p []byte) (n int, err error) {
 	w.body.Write(p)
 	return w.ResponseWriter.Write(p)
 }
 
+type idempotencyAPI struct {
+	ikCache *cache.Cache
+	handler http.HandlerFunc
+}
+
+func (i *idempotencyAPI) responseError(w http.ResponseWriter, r *http.Request, msg string, status int) {
+	if msg == "" {
+		http.StatusText(status)
+	}
+	http.Error(w, msg, status)
+}
+
+type Response struct {
+	Ready  bool // if response ready to be used
+	Header http.Header
+	Status int
+	Body   []byte
+}
+
+func (i *idempotencyAPI) getResponse(ik string) (resp *Response, err error) {
+	v, ok := i.ikCache.Get(ik)
+	if !ok {
+		err = fmt.Errorf("no valid response for ik: %s", ik)
+		return
+	}
+	resp, ok = v.(*Response)
+	if !ok {
+		err = fmt.Errorf("no valid response for ik: %s", ik)
+		return
+	}
+	return
+}
+
+func (i *idempotencyAPI) dump(prefix string) {
+	bb, _ := json.MarshalIndent(i.ikCache.Items(), "", "  ")
+	log.Printf("ikCache %s:\n%s\n", prefix, bb)
+}
+
+func (i *idempotencyAPI) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ik := r.Header.Get("Idempotency-Key")
+	if ik == "" {
+		i.responseError(w, r, "missing header: Idempotency-Key", http.StatusBadRequest)
+		return
+	}
+
+	err := i.ikCache.Add(ik, &Response{}, cache.DefaultExpiration)
+	if err != nil {
+		// ik existed, response with cached value.
+		resp, err := i.getResponse(ik)
+		if err != nil {
+			i.responseError(w, r, "", http.StatusInternalServerError)
+			return
+		}
+		for !resp.Ready {
+			time.Sleep(45 * time.Millisecond)
+
+			resp, err = i.getResponse(ik)
+			if err != nil {
+				i.responseError(w, r, "", http.StatusInternalServerError)
+				return
+			}
+		}
+		i.dump("sending cached response")
+
+		for k, v := range resp.Header {
+			w.Header()[k] = v
+		}
+		w.WriteHeader(resp.Status)
+		if resp.Body != nil {
+			w.Write(resp.Body)
+		}
+		return
+	}
+
+	i.dump("before api")
+	respWriter := &respCatcher{w, &bytes.Buffer{}, http.StatusOK}
+	i.handler(respWriter, r)
+
+	i.ikCache.Replace(ik,
+		&Response{Ready: true, Header: respWriter.Header(), Status: respWriter.statusCode, Body: respWriter.body.Bytes()},
+		cache.DefaultExpiration,
+	)
+	i.dump("after api")
+}
+
 func (app *application) idempotencyAPI(api http.HandlerFunc) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ik := r.Header.Get("Idempotency-Key")
-		if ik == "" {
-			app.responseError(w, r, "missing header: Idempotency-Key", http.StatusBadRequest)
-			return
-		}
-
-		v, ok := app.ikCache.Get(ik)
-		if ok {
-			resp := v.(*Response)
-			for !resp.Ready {
-				v, ok := app.ikCache.Get(ik)
-				if !ok {
-					app.responseError(w, r, "", http.StatusInternalServerError)
-					return
-				}
-				resp = v.(*Response)
-				time.Sleep(45 * time.Millisecond)
-			}
-
-			for k, v := range resp.Header {
-				w.Header()[k] = v
-			}
-			w.WriteHeader(resp.Status)
-			if resp.Body != nil {
-				w.Write(resp.Body)
-			}
-			return
-		}
-
-		app.ikCache.Set(ik, &Response{}, cache.DefaultExpiration)
-		dump(app.ikCache)
-
-		iw := &idempotencyWriter{w, &bytes.Buffer{}, http.StatusOK}
-		api(iw, r)
-
-		app.ikCache.Replace(ik,
-			&Response{Ready: true, Header: iw.Header(), Status: iw.statusCode, Body: iw.body.Bytes()},
-			cache.DefaultExpiration,
-		)
-		dump(app.ikCache)
-	})
+	return &idempotencyAPI{
+		ikCache: cache.New(10*time.Second, time.Minute),
+		handler: api,
+	}
 }
--- test.sh
+++ test.sh
@@ -1,10 +1,16 @@
 # Idempotency Key
 IK=$(openssl rand -hex 32)
-echo "Create song with IK=$IK"
+DATA='{"title": "title-1", "artist": "artist-1", "year": 2019}'
+echo $DATA
+IK2=$(openssl rand -hex 32)
+DATA2='{"title": "title-2", "artist": "artist-2", "year": 2021}'
 # Sending consecutive requests.
-curl -i -H "Idempotency-Key:$IK" -X POST -d '{"title": "test", "artist": "test", "year": 2021}' localhost:8080/songs/create &
-curl -i -H "Idempotency-Key:$IK" -X POST -d '{"title": "test", "artist": "test", "year": 2021}' localhost:8080/songs/create &
-curl -i -H "Idempotency-Key:$IK" -X POST -d '{"title": "test", "artist": "test", "year": 2021}' localhost:8080/songs/create
+curl -i -H "Idempotency-Key:$IK" -X POST -d "$DATA" localhost:8080/songs/create &
+curl -i -H "Idempotency-Key:$IK2" -X POST -d "$DATA2" localhost:8080/songs/create &
+curl -i -H "Idempotency-Key:$IK" -X POST -d "$DATA" localhost:8080/songs/create &
+curl -i -H "Idempotency-Key:$IK2" -X POST -d "$DATA2" localhost:8080/songs/create &
+curl -i -H "Idempotency-Key:$IK" -X POST -d "$DATA" localhost:8080/songs/create
+curl -i -H "Idempotency-Key:$IK2" -X POST -d "$DATA2" localhost:8080/songs/create &
 
 # Wait for create song done for better output.
 sleep 1
